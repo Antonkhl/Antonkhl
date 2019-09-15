9ce48b51590455f659a1f1aa0a513e4d09cdbdd2
Antonkhl <anton.khlopotin@elev.ga.ntig.se>
2019-09-15 14:17:52 +0200

    Adress till telefonnummer

--- java program/src/abc.java
+++ java program/src/abc.java
@@ -3,9 +3,23 @@ import java.util.Scanner;
 public class abc {
 public static void main(String[] args) {
 Scanner input = new Scanner(System.in);
+
 System.out.println("Username?");
 String Username = input.nextLine();
+
 System.out.println("Ålder?");
 String Ålder = input.nextLine();
+
+System.out.println("Adress?");
+String Adress = input.nextLine();
+
+System.out.println("postnummer?");
+String Postnummer = input.nextLine();
+
+System.out.println("Stad?");
+String Stad = input.nextLine();
+
+System.out.println("Telefonnummer?");
+String Telefonnummer = input.nextLine();
 }
 }
