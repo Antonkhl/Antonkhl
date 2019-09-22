5603c1c8756b6494a50ed94eed745ee222fc91a6
Antonkhl <anton.khlopotin@elev.ga.ntig.se>
2019-09-22 11:25:54 +0200

    Minsta talet

--- java program/src/abc2.java
+++ java program/src/abc2.java
@@ -66,6 +66,37 @@ if (tio>ett && tio>tva && tio>tre && tio>fyra && tio>fem && tio>sex && tio>sju &
 	System.out.println("Största talet är " + tio);
 }
 
+
+if (ett<tva && ett<tre && ett<fyra && ett<fem && ett<sex && ett<sju && ett<atta && ett<nio && ett<tio) {
+	System.out.println("Minsta talet är " + ett);
+}	
+if (tva<ett && tva<tre && tva<fyra && tva<fem && tva<sex && tva<sju && tva<atta && tva<nio && tva<tio) {
+	System.out.println("Minsta talet är " + tva);
+}
+if (tre<ett && tre<tva && tre<fyra && tre<fem && tre<sex && tre<sju && tre<atta && tre<nio && tre<tio) {
+	System.out.println("Minsta talet är " + tre);
+}
+if (fyra<ett && fyra<tva && fyra<tre && fyra<fem && fyra<sex && fyra<sju && fyra<atta && fyra<nio && fyra<tio) {
+	System.out.println("Minsta talet är " + fyra);
+}
+if (fem<ett && fem<tva && fem<tre && fem<fyra && fem<sex && fem<sju && fem<atta && fem<nio && fem<tio) {
+	System.out.println("Minsta talet är " + fem);
+}
+if (sex<ett && sex<tva && sex<tre && sex<fyra && sex<fem && sex<sju && sex<atta && sex<nio && sex<tio) {
+	System.out.println("Minsta talet är " + sex);
+}
+if (sju<ett && sju<tva && sju<tre && sju<fyra && sju<fem && sju<sex && sju<atta && sju<nio && sju<tio) {
+	System.out.println("Minsta talet är " + sju);
+}
+if (atta<ett && atta<tva && atta<tre && atta<fyra && atta<fem && atta<sex && atta<sju && atta<nio && atta<tio) {
+	System.out.println("Minsta talet är " + atta);
+}
+if (nio<ett && nio<tva && nio<tre && nio<fyra && nio<fem && nio<sex && nio<sju && nio<atta && nio<tio) {
+	System.out.println("Minsta talet är " + nio);
+}
+if (tio<ett && tio<tva && tio<tre && tio<fyra && tio<fem && tio<sex && tio<sju && tio<atta && tio<nio) {
+	System.out.println("Minsta talet är " + tio);
+}
 }
 }
 
