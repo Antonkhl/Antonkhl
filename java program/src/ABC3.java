79cdb5cf9fbce2d7a37b30e9b89ff21aa6a677f8
Antonkhl <anton.khlopotin@elev.ga.ntig.se>
2019-09-26 10:34:40 +0200

    R2 uppgiften

---
+++ java program/src/ABC3.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+public class ABC3 {
+public static void main(String[] args) {
+Scanner input = new Scanner(System.in);
+
+	
+System.out.println("Värdet på x?");
+int x = input.nextInt();
+
+System.out.println("Värdet på y?");
+int y = input.nextInt();
+int a = 0;
+
+if (x=x>a && y>a) { 
+	System.out.println("Talen ska vara i kvadrant 1");
+}
+if (x=x<a && y>a) { 
+	System.out.println("Talen ska vara i kvadrant 2");
+}
+if (x=x<a && y<a) { 
+	System.out.println("Talen ska vara i kvadrant 3");
+}
+if (x=x>a && y<a) { 
+	System.out.println("Talen ska vara i kvadrant 4");
+}
+}
+}
