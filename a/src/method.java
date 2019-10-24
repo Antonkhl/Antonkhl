ceef1e2ea9df980621244912454e0fe2960db81d
Antonkhl <anton.khlopotin@elev.ga.ntig.se>
2019-10-24 11:18:18 +0200

    Skapar methoder

---
+++ a/src/method.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class method {
+	Scanner Input = new Scanner(System.in);
+	
+	public static void main(String[] args) {
+		
+		stair(4);
+		
+	}
+	
+
+	public static void stair(int steps) {
+	for (int i = 4; i <= steps; i--) {
+		System.out.println();
+		
+	}
+		
+		
+		
+	}
+}
+	
+	
+
