8aacf0a827ed2945bc2f276671ce79010f8b87a0
Antonkhl <anton.khlopotin@elev.ga.ntig.se>
2019-10-24 11:19:54 +0200

    skapar methoder

---
+++ java program/src/volume.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+public class volume {
+	Scanner Input = new Scanner(System.in);
+
+
+public static void main(String[] args) {
+	
+	volume(40);
+}
+
+public static void volume(double radius) {
+	
+		
+	}dsfsdfsdfsdfsdfsdfsdf
+
+}
