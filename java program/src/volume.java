379392d1f3935f129612e9fa5493799a1eab845d
Antonkhl <anton.khlopotin@elev.ga.ntig.se>
2019-10-24 11:53:32 +0200

    Kommentarer

--- java program/src/volume.java
+++ java program/src/volume.java
@@ -2,20 +2,17 @@ import java.util.Scanner;
 public class volume {
 	Scanner Input = new Scanner(System.in);
 
-
 public static void main(String[] args) {
-	
-	volume(20.0);
+	volume(20.0);             
 }
-
 public static double volume(double radius) {
 	
-	double radius1 = radius*radius*radius;
-	double radius2 = 4*3.14*radius1;
-	double volome1 = radius2/3;
+	double Fullradius = radius*radius*radius; //Jag får ut den fullständiga radien så att jag kan senare använda den för min ekvation.
+	double volume1 = 4*3.14*Fullradius; //Jag räknar ut 
+	double Final = volume1/3; // Sen dividerar jag på 3 för att få min slut volym
 	
-	System.out.println(volome1);
+	System.out.println(Final);
 	
-	return 1;
+	return 3;
 	}
 }
