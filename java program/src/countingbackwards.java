6ba92574e576e350739922bba23b6ae7e4852f9b
Antonkhl <anton.khlopotin@elev.ga.ntig.se>
2019-10-27 09:28:45 +0100

    skapar båda methoder

---
+++ java program/src/countingbackwards.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+public class countingbackwards {
+Scanner Input = new Scanner(System.in);
+
+
+public static void main (string[] args) {
+	reverse(word); 
+}
+ 
+public static String reverse(string str) {
+	
+	
+
+}
+
+
+
+}
