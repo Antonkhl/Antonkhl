5a2599884163f70a5adf9f846a639d674e556a4b
Antonkhl <anton.khlopotin@elev.ga.ntig.se>
2019-10-27 10:24:57 +0100

    skapar methoder

---
+++ java program/src/ord.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+public class ord {
+	Scanner Input = new Scanner(System.in);
+	
+	public static void main(String[] args) {
+		
+	
+	}
+	
+	public static int count(String str, char c) {
+		
+		
+	}
+	
+	
+	
+}
