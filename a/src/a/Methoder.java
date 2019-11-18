5f03f024249e9c636b0b56dfe867bf21a621b61f
Antonkhl <anton.khlopotin@elev.ga.ntig.se>
2019-11-18 10:05:53 +0100

    Importerar mina enums och göra den 3dje methoden

--- a/src/a/Methoder.java
+++ a/src/a/Methoder.java
@@ -2,9 +2,15 @@ package a;
 
 public class Methoder {
 	
+	static double G = 9.82;
+	static double p_0 = 1000;
+	static double c = 299792458;
+	
+	
 	public static void main(String[] args) {
 		System.out.println(fahrenheitToCelcius(50)); // Anropar värdet för fahrenheit
 		System.out.println(kelvinToCelsius(0)); // Anropar värdet för kelvin
+		System.out.println(fluidPressure(FluidTable.WATER, 10));
 	}
 	
 	public static double fahrenheitToCelcius(double fahrenheit) {
@@ -15,6 +21,12 @@ public class Methoder {
 	public static double kelvinToCelsius(double kelvin) {
 		double KetoCe = kelvin - 273.15; // Använder formlen för att omvandla värden
 		return KetoCe; //Returnar värdet
+	}
+	
+	public static double fluidPressure(FluidTable fluid, double deep) {
+		double FluPre = G*fluid.density*deep;
+		return FluPre;
+		
 	}
 	
 
