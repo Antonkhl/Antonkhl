a5f74b9d79a597854aef4a7a2bf62b04e8397eda
Antonkhl <anton.khlopotin@elev.ga.ntig.se>
2020-03-17 14:53:53 +0100

    Bestämmer alla namn på methoder

---
+++ Uppgifter/src/talspelet.java
@@ -0,0 +1,31 @@
+public class talspelet {
+
+	public static void main(String[] args) {
+		
+	}
+	
+	
+	
+	public static String playersName() {
+		
+	}
+	
+	public static int minMaxNumbers() {
+		
+	}
+	
+	public static int limitOnTries() {
+		
+	}
+	
+    public static void mainGame() {
+		
+	}
+    
+    public static afterGame() {
+    	
+    }
+    
+	
+
+}
