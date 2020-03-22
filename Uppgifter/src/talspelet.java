88b1347fca5b982323dcfa2b37d2478a1a3e0f58
Antonkhl <anton.khlopotin@elev.ga.ntig.se>
2020-03-22 10:31:42 +0100

    mainggame progress och anropar olika variabler

--- Uppgifter/src/talspelet.java
+++ Uppgifter/src/talspelet.java
@@ -1,25 +1,21 @@
 import java.util.Scanner;
+import java.util.Random;
 
 public class talspelet {
-
-	public static void main(String[] args) {
-		System.out.println(playersName("name"));
-		System.out.println(minMaxNumbers(3, 4));
-		
-
-	}
 	
 	
-	
-	public static String playersName(String namn) {
+	public static void playersName(String namn) {
 		System.out.println("Hej och välkommen till mitt talspel! Idag ska vi få se om du har vad som krävs för att gissa tal, men innan vi börjar, vad heter du?");
 		Scanner input = new Scanner(System.in);
 	    namn = input.nextLine();
 		System.out.println("Hej" + namn + "och jag hoppas att du har det roligt!");
-		return namn;
+		
+		minMaxNumbers(namn,0,0,0);
+		
+
 	}
 	
-	public static int minMaxNumbers(int minNumber, int maxNumber) {
+	public static void minMaxNumbers(String namn, int minNumber, int maxNumber, int slumpTal) {
 		System.out.println("Innan jag berättar regler för spelet vill jag att ni anger max och min tal för spelet. Det är mellan dessa två tal som talet kommer slumpas mellan");
 		
 		System.out.println("Börja med att ange mintalet");
@@ -29,11 +25,17 @@ public class talspelet {
 		System.out.println("Ange nu maxtalet");
 	    maxNumber = input.nextInt();
 	    
-	    return minNumber;
+	    Random random = new Random();
+	    slumpTal = random.nextInt(maxNumber - minNumber +1) + minNumber;
+	    
+	    limitOnTries(0,namn,slumpTal);
+	    
+	  
+	
 		
 	}
 	
-	public static void limitOnTries(int numberOfTries) {
+	public static void limitOnTries(int numberOfTries, String namn, int slumpTal) {
 		String svar1 = "Ja";
 		String svar2 = "Nej";
 
@@ -49,19 +51,52 @@ public class talspelet {
 		}
 		
 		if (limit.equals(svar2)) {
-			System.out.println("Ok, då går vi vidare!");
+			System.out.println("Ok " + namn + ", då går vi vidare!");
+			numberOfTries = 3251;
 		}
 		
 		else {
 			System.out.println("Jag vill att du svarar Ja eller Nej");
 		}
 		
+		mainGame(0,namn,numberOfTries,slumpTal,0);
+		
 		
 	}
 	
 	
-    public static void mainGame() {
+    public static void mainGame(int gissning, String namn, int numberOfTries, int slumptal, int substration) {
+		System.out.println("Ok Innan vi börjar ska jag berätta för dig exakt hur spelet fungerar, "
+				+ "Jag kommer att slumpa ett tal mellan ditt minnumber och maxnumber, "
+				+ "Du ska gissa ett tal mellan dom två intervaler och beroende på det talet du gissar kommer jag antigen säga mindre eller mer"
+				+ "Om du har begränsning på försök kommer jag ochså meddela hur många försök du har kvar"
+				+ "När spelet är slut kommer jag ger dig all statistik som du behöver"
+				+ "Med det sagt, kan vi köra igång!");
+		Scanner input = new Scanner(System.in); 
+		gissning = input.nextInt();
 		
+		while (gissning != slumptal ) {
+			substration = gissning - slumptal;
+			
+			if (substration>0) {
+				System.out.println("mindre");
+			}
+			
+			if (substration<0) {
+				System.out.println("Mer")
+			}
+			
+			
+			
+			
+		}
+		
+		
+		
+		
+    	
+    	
+    	
 	}
     
     public static void afterGame() {
